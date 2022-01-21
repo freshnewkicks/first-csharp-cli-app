5884242921ab7caa4cd156681d89bb65efdaf140
freshnewkicks <tylerdukedev@gmail.com>
2022-01-21 07:23:57 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace first_csharp_cli_app
+{
+    class ReadFromFile
+    {
+        static void Main()
+        {
+            // read file as one string
+            string[] jsonInput = System.IO.File.ReadAllLines(@"C:\Users\Tyler\Desktop\Dev\csharp\first-csharp-cli-app\io-sample-json.json");
+
+            foreach (string lines in jsonInput)
+            {
+                Console.WriteLine("\t" + lines);
+            }
+
+        }
+    }
+}
